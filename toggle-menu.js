8a3cc62e35c1372c8f9b694a90fe482fd230df6c
320-gogo32 <133937080+320-gogo32@users.noreply.github.com>
2023-09-06 13:19:27 +0900

    JavaScript 更新

--- toggle-menu.js
+++ toggle-menu.js
@@ -1,17 +1,25 @@
 window.addEventListener('load', function () {
-    var $button = document.querySelector('.toggle-menu-button');
-    var $menu = document.querySelector('.header-site-menu');
-    $button.addEventListener('click', function () {
-        if ($menu.classList.contains('is-show')) {
-            $menu.classList.remove('is-show');
+    var button = document.querySelector('.toggle-menu-button');
+    var menu = document.querySelector('.header-site-menu');
+    var navs = document.querySelectorAll('.header-site-menu li');
+
+    // ハンバーガーメニューのアクション
+    button.addEventListener('click', function () {
+        if (menu.classList.contains('is-show')) {
+            menu.classList.remove('is-show');
         }
         else {
-            $menu.classList.add('is-show');
+            menu.classList.add('is-show');
         }
-
-        // ボタンがクリックされた後にメニューを非表示にする
-        if (!$menu.classList.contains('is-show')) {
-            $menu.style.display = 'none';
-        }
+    });
+    
+    // ハンバーガーメニュー内に格納されたヘッダーメニューのアクション
+    navs.forEach(element => {
+        element.addEventListener('click', function () {
+            if (menu.classList.contains('is-show')) {
+                menu.classList.remove('is-show');
+            }
+        })
+        
     });
 });
